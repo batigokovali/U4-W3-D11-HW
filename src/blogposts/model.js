c0a5531b4faf5e616e0bd3419d977fb07514f53a
Batı Gökovalı <batigokovali@icloud.com>
2023-03-15 12:09:59 +0300

    update

--- src/blogposts/model.js
+++ src/blogposts/model.js
@@ -7,10 +7,22 @@ const blogpostsSchema = new Schema(
         category: { type: String, required: true },
         title: { type: String, required: true },
         cover: { type: String, required: true },
-        "readTime.value": { type: Number, required: true },
-        "readTime.unit": { type: String, required: true },
-        "author.name": { type: String, required: true },
-        "author.avatar": { type: String, required: true },
+        readTime: {
+            value: { type: Number, required: true },
+            unit: {
+                type: String, required: true,
+                validate: {
+                    validator: function (unit) {
+                        return ["seconds", "minutes", "hours", "years"].includes(unit);
+                    },
+                    message: "Unit must be one of 'seconds', 'minutes','hours' oe 'years'!",
+                },
+            }
+        },
+        author: {
+            name: { type: String, required: true },
+            avatar: { type: String, required: true },
+        },
         content: { type: String, required: true }
     },
     {
